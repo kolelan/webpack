32ddad27458e719f5eec5f74fdf06b5a4bf9bc11
Kolelan <kolelan@yandex.ru>
2019-06-19 18:21:29 +0300

    Работа с webpack. 3 способа импорта

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-// import {config} from './modules/config';
-// import AppService from './modules/app.service';
-// import './modules/header.component';
-import _ from 'lodash';
-// function component(){
-//     const el = document.createElement('div');
-//     el.innerHTML = _.join(['Hello','webpack'],' ');
-//     return el;
-// }
-// document.body.appendChild(component());
+import {config} from './modules/config';
+import AppService from './modules/app.service';
+import './modules/header.component';
 
-// console.log('Config key: ',config.key);
-// const service = new AppService('Hello allo');
-// service.log();
-
-var array = [1,2,5,16,28,4,2,1,6];
-var array2 = [1,false,5,16,null,4,2,undefined,'',6];
-console.log('chunk: ',_.chunk(array,7));
-console.log('array: ',array);
-console.log('compact: ',_.compact(array2));
-console.log('array: ',array2);
-console.log('dropRight: ',_.dropRight(array,3));
-console.log('array: ',array);
-console.log('dropRightWhile: ',_.dropRightWhile(array,function (item) {
-    return item<10;
-}));
-console.log('array: ',array);
-console.log('fill: ',_.fill(array,'*',2,7));
-console.log('array: ',array);
+console.log('Config key: ',config.key);
+const service = new AppService('Hello allow');
+service.log();
