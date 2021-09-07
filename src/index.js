415c9102c6eb2e737d46d47a8d86b3f4bb21db1b
gstepenko <g.stepenko@rusbitech.ru>
2021-09-07 23:45:04 +0300

    Незначительные изменения, Увеличена версия вебпака и лодаш, изменил выходные параметры, добавил мейкфайл, добавил импорт lodash для просмотра в консоли

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {config} from './modules/config';
 import AppService from './modules/app.service';
 import './modules/header.component';
+import _ from 'lodash';
 
 console.log('Config key: ',config.key);
 const service = new AppService('Hello allow');
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+// Подключим встроенный модуль в вебпак, который называется path
 const path = require('path');
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname,'dist')
     }
 };
